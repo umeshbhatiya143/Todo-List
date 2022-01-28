59f38fabc0f6b57bcd9dd97858fd39f9ec6cb9c3
umeshbhatiya143 <95590920+umeshbhatiya143@users.noreply.github.com>
2022-01-29 02:58:45 +0530

    Todo List for college projects

---
+++ Todo List1/src/MyComponents/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+  <div>
+      This is an about component
+      <p>1st Example: Basic Routing
+      In this example we have 3 “pages” 
+      handled by the router: a home page,
+       an about page, and a users page. As
+        you click around on the different s, 
+        the router renders the matching .Note: 
+        Behind the scenes a  renders an  with a 
+        real href, so people using the keyboard 
+        for navigation or screen readers will still 
+        be able to use this app.</p>
+  </div>
+  )
+};
